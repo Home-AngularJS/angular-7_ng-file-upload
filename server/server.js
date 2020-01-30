58f3ecf6e4f94e584dc7c7bd48ec5c3b7f2e9c94
Lazarchuk <a.lazarchuk@cartsys.com.ua>
2020-01-30 15:17:59 +0200

    #release

--- server/server.js
+++ server/server.js
@@ -7,11 +7,24 @@ const app = express();
 const PORT = 5000;
 const URL = `http://localhost:${PORT}/`;
 
+/**
+ * @see https://www.geeksforgeeks.org/how-to-add-sleep-wait-function-before-continuing-in-javascript/
+ */
+function sleep(milliseconds) {
+  let timeStart = new Date().getTime();
+  while (true) {
+    let elapsedTime = new Date().getTime() - timeStart;
+    if (elapsedTime > milliseconds) {
+      break;
+    }
+  }
+}
+
 app.use(express.static('public'))
 
 var storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        const dir = './public/images/uploads';
+        const dir = './public/files/uploads';
         mkdirp(dir, err => cb(err, dir))
     },
     filename: (req, file, cb) => {
@@ -24,12 +37,16 @@ const upload = multer({ storage })
 app.use(cors());
 
 app.post('/upload', upload.single('image'), (req, res) => {
-    if (req.file) {
-        res.json({imageUrl: `${URL}images/uploads/${req.file.filename}`});
-    }
-    else{
-        res.status("409").json("No Files to Upload.");
-    } 
+  console.log('<<<<< ' + new Date)
+  sleep(1000) // Set one second pause from server response
+  if (req.file) {
+    res.json({fileUrl: `${URL}files/uploads/${req.file.filename}`, status: "OK"});
+    res.status(200);
+  } else {
+    res.status("409").json("No Files to Upload.");
+    res.json({fileUrl: "No Files to Upload.", status: "ERROR"});
+  }
+  console.log('>>>>> ' + new Date)
 });
 
 app.listen(PORT);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpEventType } from '@angular/common/http';
-import { moveItemInArray, transferArrayItem, CdkDragDrop } from '@angular/cdk/drag-drop';
 import { Observable } from 'rxjs';
 
 class ItemFile {
   file: File;
   uploadProgress: string;
+  uploadStatus: string;
 }
 
 @Component({
@@ -28,7 +28,7 @@ export class AppComponent {
     let files: FileList = event.target.files;
     for (let i = 0; i < files.length; i++) {
       // if (files.item(i).name.match(/\.(jpg|jpeg|png|gif)$/)) {
-        this.items.push({file: files.item(i), uploadProgress: '0'});
+        this.items.push({file: files.item(i), uploadProgress: '0', uploadStatus: ''});
       // }
     }
     this.message = `${this.items.length} valid image(s) selected`;
@@ -53,23 +53,33 @@ export class AppComponent {
     });
   }
 
-  uploadFile(item) {
+  uploadFile(item: ItemFile) {
     const formData = new FormData();
     formData.append('image', item.file, item.file.name);
     return this.http.post('http://localhost:5000/upload', formData, {
       reportProgress: true,
       observe: 'events'
-    })
-      .subscribe(event => {
-        if (event.type === HttpEventType.UploadProgress ) {
-          item.uploadProgress = `${(event.loaded / event.total * 100)}%`;
-        }
+    }).subscribe(event => {
+        if (event.type === HttpEventType.UploadProgress ) item.uploadProgress = `${(event.loaded / event.total * 100)}%`;
         if (event.type === HttpEventType.Response) {
-          // this.imageUrls.push(event.body.imageUrl);
+          let body: any = event.body;
+          // let fileUrl: string = body.fileUrl
+          // let fileName = fileUrl.replace( 'http://localhost:5000/files/uploads/', '')
+          // fileName = fileName.substring(14, fileUrl.length)
+          // console.log('Загрузка файла "' + fileName + '" успешно завершена!')
+          console.log('Загрузка файла "' + item.file.name + '" успешно завершена!')
+          let status: string = body.status
+          item.uploadStatus = status
         }
-      });
+      }, error => {
+      // alert( JSON.stringify(error) );
+      console.log('Ошибка загрузки файла: ' + item.file.name)
+    });
   }
 
+  /**
+   * @see https://stackoverflow.com/questions/15453979/how-do-i-delete-an-item-or-object-from-an-array-using-ng-click
+   */
   removeFile(item: ItemFile) {
       this.items.splice(this.items.indexOf(item),1);
   }
